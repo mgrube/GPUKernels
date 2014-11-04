0f4f79a302b68ebc86d0ef8650b0a5eada1c4fe4
Michael Grube <michael.grube@gmail.com>
2014-11-04 15:58:26 +0000

    reading from file

--- src/Kernels/PCA.py
+++ src/Kernels/PCA.py
@@ -1,21 +1,14 @@
 import pyopencl as cl
 
 platform = cl.get_platforms()[0]
-device = cl.get_deivces()[0]
+device = platform.get_devices()[0]
 context = cl.Context([device])
 
+clfile = open('test.cl', 'rb')
+
 #Build and describe the kernel in one swoop
-program = cl.Program(context, programstring).build()
+program = cl.Program(context, clfile.read()).build()
 
 queue = cl.CommandQueue(context)
 
-mem_flags = cl.mem_flags
-
-matrix_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=matrix)
-
-vector_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=vector)
-
-matrix_dot_vector = numpy.zeros(4, numpy.float32)
-
-destination_buf = cl.Buffer(context, mem_flags.WRITE_ONLY, matrix_dot_vector.nbytes)
-     
+    
